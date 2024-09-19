5d42ce2ec9a9950643e4635805c332223917f1d1
soko.christian.echuaka <soko.christian.echuaka@auleseebe.upc.edu>
2024-09-19 11:33:58 +0200

    ..

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,5 @@
 #Codigo para problema 2
+rm(list=ls())
+data=mtcars
+mis_dades=mtcars
+hist(mis_dades$mpg)
--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,27 @@
 #Codigo para problema 3
+
+X=c(-8,5,2,-8,9,5,2,-3,1,-1,4,-4,9,3,-9,7,0,-7,8,-4,1,7,-6,4,5,-9,-2,-8,5,-5)
+
+sum(X)
+
+diffY=c(21,2,26)
+Y=X
+
+for(i in diffY){
+  Y=Y[-i]
+}
+
+SUM_N=0
+SUM_M=0
+
+for(n in X)
+{
+  SUM_N = SUM_N + exp(n)
+}
+for(m in Y){
+  SUM_M=SUM_M + exp(m)
+}
+
+Sumatorio = SUM_N + SUM_M
+
+Y[19]>=Y[24]
