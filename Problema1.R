346a1d1d78fb1057c732fa1470238c05b8d60968
soko.christian.echuaka <soko.christian.echuaka@auleseebe.upc.edu>
2024-09-19 11:09:30 +0200

    .

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,32 @@
 #Codigo para problema 1
 
-plot(1:5)
+mis_dades<-mtcars
+mis_dades$qsec
+
+mean(mis_dades$qsec)
+
+median(mis_dades$drat)
+
+#primer cuartil
+quantile(mis_dades$drat)[1]
+
+quantile(mis_dades$mpg,0.18)
+quantile(mis_dades$mpg,0.82)
+
+#rango interquartil
+IQR(mis_dades$cyl)
+
+#desviacion tipica
+sd(mis_dades$cyl)
+
+#variancia
+var(mis_dades$qsec)
+
+
+
+#hacer una tabla de frecuencia
+
+
+intervalos_qsec<-cut(mis_dades$qsec,4)
+table(intervalos_qsec)
+hist(mis_dades$qsec,10)
